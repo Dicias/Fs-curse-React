198c61346128c13d7ed91e1ab40cba13ede0376f
dicias <dmacias26@alumnos.uaq.mx>
2023-06-25 21:08:23 -0600

    change url var

--- src/App.jsx
+++ src/App.jsx
@@ -62,7 +62,7 @@ const handleNoteChange = (event) =>{
 }
 
 const toggleImportanceOf = (id) =>{
-const url = `http://localhost:3001/api/notes/${id}`
+const url = `api/notes/${id}`
 const note = notes.find(n => n.id === id)
 const changedNote = {...note, important: !note.important}
 
